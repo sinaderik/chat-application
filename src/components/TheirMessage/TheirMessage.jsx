7b29b48f1ce50f5712224526031f2bb8396b4ade
sina <sinaderik7@gmail.com>
2024-03-24 16:19:07 +0430

    feat: TheirMessage components logic implemented

--- src/components/TheirMessage/TheirMessage.jsx
+++ src/components/TheirMessage/TheirMessage.jsx
@@ -1,7 +1,35 @@
 import React from 'react'
 
-export default function TheirMessage() {
+export default function TheirMessage({ message, lastMessage }) {
+
+  const isFirstMessageByUser = !lastMessage || lastMessage.sender.username !== message.sender.username
   return (
-    <div>TheirMessage</div>
+    <div className='message-row'>
+      {isFirstMessageByUser && (
+        <div
+          className='message-avatar'
+          style={{ backgroundImage: `url(${message?.sender?.avatar})` }}
+        />
+      )}
+      {
+        message?.attachments?.length > 0
+          ? (
+            <img
+              src={message.attachments.file[0]}
+              alt='message-attachments'
+              className='message-image'
+              style={{ marginLeft: isFirstMessageByUser ? '4px' : '48px' }}
+            />
+          ) : (
+            <div
+              className='message'
+              style={{ float: "left", backgroundColor: "#CABCDC", marginLeft: isFirstMessageByUser ? '4px' : '48px' }}
+            >
+              {message.text}
+            </div>
+          )
+
+      }
+    </div>
   )
 }
