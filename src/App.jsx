933c14283517e4d2beb7608fe390da202d198bfb
sina <sinaderik7@gmail.com>
2024-03-25 23:12:58 +0430

    feat: authentication process completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,20 @@ import React from 'react'
 import "./App.css"
 import { ChatEngine } from 'react-chat-engine'
 import ChatFeed from './components/ChatFeed/ChatFeed'
+import LoginForm from './components/LoginForm/LoginForm'
 
 export default function App() {
+
+  if (!localStorage.getItem('username')) return <LoginForm />
+
   return (
     <>
-      
+
       <ChatEngine
         height="100vh"
         projectID="555fa585-9abf-433d-9a6b-77714d75f5c1"
-        userName="sina"
-        userSecret="123456"
+        userName={localStorage.getItem("username")}
+        userSecret={localStorage.getItem("password")}
         renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
       />
 
--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,18 +1,36 @@
 import React, { useState } from 'react'
 import axios from 'axios'
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
 
 export default function LoginForm() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("")
+    const [error, setError] = useState("")
+    const [loading, setLoading] = useState(false)
 
-    function handleSubmit() {
+    async function handleSubmit(e) {
+        e.preventDefault();
 
+        const authObject = { 'Project-ID': "555fa585-9abf-433d-9a6b-77714d75f5c1", 'User-Name': username, 'User-Secret': password };
+        try {
+            setError("")
+            setLoading(true)
+            await axios.get("https://api.chatengine.io/chats", { headers: authObject })
+            localStorage.setItem("username", username)
+            localStorage.setItem("password", password)
+            setLoading(false)
+            window.location.reload();
+        } catch {
+            setLoading(false)
+            setError("Opps, incorrect username or password")
+        }
     }
 
     return (
         <div className='wrapper'>
             <div className='form'>
-                <h1>Chat Application</h1>
+                <h1 style={{ color: "white", textAlign: "center", marginBottom: "1rem" }}>Chat Application</h1>
                 <form onSubmit={handleSubmit}>
                     <input
                         className='input'
@@ -32,9 +50,17 @@ export default function LoginForm() {
                     />
                     <div align='center'>
                         <button className='button' type='submit'>
-                            <span>Login</span>
+                            <span style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+                                Login
+                                {loading
+                                    ? <Box sx={{ display: 'flex', marginLeft: "0.5rem", fontSize: "20px" }}>
+                                        <CircularProgress sx={{ fontSize: "20px" }} />
+                                    </Box>
+                                    : ""}
+                            </span>
                         </button>
                     </div>
+                    <h2 className='error'>{error}</h2>
                 </form>
             </div>
         </div>
